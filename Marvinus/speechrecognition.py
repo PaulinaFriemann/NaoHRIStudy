f679fd7075cfcd78e16ac726d7cefa16763d6309
Marc <m.j.m.degroot@student.ru.nl>
2016-06-16 09:01:32 +0200

    More speech options
    still need more for neutral, and a bit more for 6 and 7.

--- Marvinus/speechrecognition.py
+++ Marvinus/speechrecognition.py
@@ -5,6 +5,7 @@ Created on Thu May 26 13:19:36 2016
 @author: Gast1
 """
 from naoqi import ALProxy
+from random import randint
 import time
 
 
@@ -47,18 +48,68 @@ class SpeechRecognition:
             print "Interrupted by user"
             print "Stopping..."
         
-    def react(self, word):
-        if word == "zero" or word == "one" or word =="two" or word == "three" :
-            self.speakProxy.say("Was it that bad?")
-        elif word == "four" or word == "five" or word == "six" or word == "seven":
-          self.speakProxy.say("I will try better next time")
-        elif word == "eight" or word == "nine":
-             self.speakProxy.say("You like that? Wait until you hear this one!")
-        elif word == "ten":
-             self.speakProxy.say("I am so glad you liked it, I worked on this for several months. I am actually thinking of performing next month in theatre, would you come visit me?")
+    def react(self, word, condition):
+        lottery = randint(1,3)      
+if condition == 'neutral':    
+    
+            if word == "zero":        
+                self.speakProxy.say("You are making a joke, right? Please tell me that was a joke.")
+                self.speakProxy.say("Are you trying to make me sad? ")
+                self.speakProxy.say("Well, you try to play a xylophone without proper hands.")
+            elif word == "one" or word =="two" or word == "three" :
+                if lottery = 1:
+                    self.speakProxy.say("Was it that bad?")
+                elif lottery = 2:
+                    self.speakProxy.say("Maybe I should stop playing.")
+                elif lottery = 3:
+                    self.speakProxy.say("I am sorry I wasted your time.")
+            elif word == "four" or word == "five":
+                if lottery = 1:
+                    self.speakProxy.say("I will try better next time")
+                elif lottery = 2:
+                    self.speakProxy.say("Something must have gone wrong")
+                elif lottery = 3:
+                    self.speakProxy.say("Sorry, I am still learning.")            
+            elif word == "six":
+                if lottery = 1:
+                self.speakProxy.say("The piece is difficult, but I am glad I am pulled it off.")
+                elif lottery = 2:
+                self.speakProxy.say("You gave me a six!")
+                elif lottery = 3:    
+                self.speakProxy.say("Thanks! I am getting better!")
+            elif word == "seven":
+                if lottery = 1:
+                    self.speakProxy.say("I am glad you sort of liked it!")
+                elif lottery = 2:
+                    self.speakProxy.say("I will be a professional one day!")
+                elif lottery = 3:
+                    self.speakProxy.say("You gave me a seven!")
+                
+            elif word == "eight" or word == "nine":
+                if lottery = 1:
+                 self.speakProxy.say("You like that? Wait until you hear this one!")
+                elif lottery = 2:
+                 self.speakProxy.say("This is my favourite piece as well!")
+                elif lottery = 3:
+                 self.speakProxy.say("I am so happy my efforts payed off and that you liked it!")
+            elif word == "ten":
+                if lottery = 1:
+                 self.speakProxy.say("I am so glad you liked it, I worked on this for several months. I am actually thinking of performing next month in theatre, would you come visit me?")
+                elif lottery = 2:
+                 self.speakProxy.say("You are too kind!")
+                elif lottery = 3:
+                 self.speakProxy.say("Was it that good? Maybe I will start composing my own songs!")
+            else:
+                self.speakProxy.say("I do not understand you. Could you repeat what you just said?")
+                self.speakProxy.say("I am sorry, I did not hear what you just said.")
+                self.speakProxy.say("Would you mind to repeat what you just said?")
         else:
-            self.speakProxy.say("I do not understand you. Could you repeat what you just said?")
-            
+                if lottery = 1:
+                 self.speakProxy.say("Would you like to hear another song?")
+                elif lottery = 2:
+                 self.speakProxy.say("Thank you for your support.")
+                elif lottery = 3:
+                 self.speakProxy.say("Thank you for rating me.")
             
     def stop_recognition(self):
         self.speechRecProxy.unsubscribe("ASR")
